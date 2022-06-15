3512e291fdb11679d4678d6842a112a53abc395c
h-hjaffery <hasnain.jafferyy@gmail.com>
2022-06-15 18:13:50 +0800

    interests component completed

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
     return (
         <div className='about'>
             <h1>About</h1>
-            <p>Currently a student in computer science with a particular interest in front-end development. Always striving to push myself in pursuit of real world knowledge and investing in myself. Unfailingly excited about the journey moving forward.</p>
+            <p>Currently a student in computer science with a particular interest in front-end development. Always striving to push myself in pursuit of real world knowledge and self investment. Unfailingly excited about the journey moving forward.</p>
         </div>
     )
 }
--- src/components/Interests.js
+++ src/components/Interests.js
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Interests() {
     return (
         <div className='interests'>
-            Interests stuff goes here
+            <h1>Interests</h1>
+            <p>I enjoy staying active. This can include playing tennis, going to the gym or any other activity that piques my interest. In my free time I try to find a good balance between learning new skills or reading books, or simply relaxing and resting myself.</p>
         </div>
     )
 }
