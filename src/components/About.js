d175b9904fc2cb24c01cabe8c9b5d81b09908b96
h-hjaffery <hasnain.jafferyy@gmail.com>
2022-06-15 17:53:07 +0800

    about section completed

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,8 @@ import React from 'react'
 export default function About() {
     return (
         <div className='about'>
-            About stuff goes here
+            <h1>About</h1>
+            <p>Currently a student in computer science with a particular interest in front-end development. Always striving to push myself in pursuit of real world knowledge and investing in myself. Unfailingly excited about the journey moving forward.</p>
         </div>
     )
 }
