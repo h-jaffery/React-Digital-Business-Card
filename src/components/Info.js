396a827f8f3169dd20f12fb4b50db6e50014f349
h-hjaffery <hasnain.jafferyy@gmail.com>
2022-06-15 17:21:29 +0800

    initial draft of info section complete

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import PersonImage from '../images/testimage.png'
+import Mail from '../images/mail.svg'
+import LinkedIn from '../images/linkedin.svg'
 
 export default function Info() {
     return(
@@ -7,7 +9,21 @@ export default function Info() {
             <img src={PersonImage} alt='not me'/>
             <h1>Hasnain Jaffery</h1>
             <h2>Student</h2>
-            <h3>hasnainjafery.website</h3>
+            <h3>hasnainjaffery.website</h3>
+            <div className='info--buttons'>
+                <a href='mailto: hasnain.jafferyy@gmail.com'>
+                    <button type="button" className='email--button'>
+                        <img src={Mail} alt='mail' className='button--icon'></img>
+                        <span>Email</span>
+                    </button>
+                </a>
+                <a href='https://www.linkedin.com/in/hasnain-jaffery/'>
+                    <button type="button" className='linkedin--button'>
+                        <img src={LinkedIn} alt='mail' className='button--icon'></img>
+                        <span>LinkedIn</span>
+                    </button>
+                </a>
+            </div>
         </div>
     )
 }
